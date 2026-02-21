5143290e367328253c22309780f3e8628c8ec6ad
Minho Kang <busster@naver.com>
2026-02-21 14:19:11 +0900

    securityConfig를 jwt 방식에 맞게 기초 설정, 아무 api를 요청했을 때 로그인 폼이 안 떠야 성공 스프링 시큐리티는 기본적으로 => 세션을 생성함, 기본 로그인 페이지 만듦, csrf 활성화되어 있음 그래서 jwt방식에 맞게 꺼주는 것임

--- src/main/java/com/login01/controller/MainController.java
+++ src/main/java/com/login01/controller/MainController.java
@@ -1,9 +1,14 @@
 package com.login01.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
-	
+	@GetMapping("/ho")
+	public String dasf() {
+		
+		return "fdsfa";
+	}
 }
---
+++ src/main/java/com/login01/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.login01.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	
+	@Bean
+	public SecurityFilterChain FilterChain(HttpSecurity http)throws Exception {
+		
+		http
+		.csrf(csrf -> csrf.disable()) // 일단 끔 (JWT 연습 단계)
+        .sessionManagement(session ->
+            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .formLogin(form -> form.disable())
+        .httpBasic(basic -> basic.disable());
+		
+		
+		return http.build();
+	}
+}

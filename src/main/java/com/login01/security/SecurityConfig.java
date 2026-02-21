dfd921bb3c0cfd6f13affbf2195e2e7b2d4ea08d
Minho Kang <busster@naver.com>
2026-02-21 17:46:26 +0900

    cors 정책 추가 후 리액트에서 로그인 요청 시 잘 되는 것 확인 완료

--- src/main/java/com/login01/security/SecurityConfig.java
+++ src/main/java/com/login01/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.login01.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,6 +12,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,6 +28,7 @@ public class SecurityConfig {
 	public SecurityFilterChain FilterChain(HttpSecurity http)throws Exception {
 		
 		http
+		.cors(cors -> cors.configurationSource(corsConfigurationSource()))
 		.csrf(csrf -> csrf.disable()) // 일단 끔 (JWT 연습 단계)
         .sessionManagement(session ->
             session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -42,6 +48,26 @@ public class SecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 	    return new BCryptPasswordEncoder();
+	}
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+
+	    CorsConfiguration configuration = new CorsConfiguration();
+
+	    // 프론트 주소 (개발용)
+	    configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+	    configuration.setAllowedMethods(
+	        List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
+	    );
+	    configuration.setAllowedHeaders(
+	        List.of("Authorization", "Content-Type")
+	    );
+	    configuration.setAllowCredentials(true);
+	    UrlBasedCorsConfigurationSource source =
+	        new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+
+	    return source;
 	}
 	
 	

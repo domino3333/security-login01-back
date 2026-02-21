4ae188f293397851bf3a114baa44b29f131559a9
Minho Kang <busster@naver.com>
2026-02-21 14:07:56 +0900

    프로젝트 초기화 / 서버 구동 확인

---
+++ src/main/java/com/login01/SecurityLogin01Application.java
@@ -0,0 +1,13 @@
+package com.login01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityLogin01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityLogin01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/login01/SecurityLogin01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityLogin01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

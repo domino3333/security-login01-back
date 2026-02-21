bcd3843c25cd57488c7c114be14aa9930dd4f440
Minho Kang <busster@naver.com>
2026-02-21 16:13:18 +0900

    멤버에 시퀀스 연결해주기

--- src/main/java/com/login01/domain/Member.java
+++ src/main/java/com/login01/domain/Member.java
@@ -1,17 +1,32 @@
 package com.login01.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
 
-    @Id
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq")
+	@SequenceGenerator(
+	    name = "member_seq",
+	    sequenceName = "member_seq",
+	    allocationSize = 1
+	)
+	private Long id;
 
-    private String email;
-    private String password;
-    private String role;
+	private String email;
+	private String password;
+	private String role;
 }

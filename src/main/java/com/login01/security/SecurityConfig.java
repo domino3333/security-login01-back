9d270bf30ceaa001f3cb1eba55f7103fd6e79a51
Minho Kang <busster@naver.com>
2026-02-21 15:21:59 +0900

    2단계)authentication 매니저 추가 1단계는 UsernamePasswordAuthenticationFilter 구현이지만 jwt 방식에서는 필요없다

--- src/main/java/com/login01/security/SecurityConfig.java
+++ src/main/java/com/login01/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.login01.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,4 +30,19 @@ public class SecurityConfig {
 		
 		return http.build();
 	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(
+	        AuthenticationConfiguration configuration) throws Exception {
+	    return configuration.getAuthenticationManager();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }

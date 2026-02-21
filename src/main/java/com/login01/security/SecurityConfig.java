c0cb7e5dbe9310ed2f6d3e1a29d457109f2b9f6d
Minho Kang <busster@naver.com>
2026-02-21 15:31:32 +0900

    password encoder 추가

--- src/main/java/com/login01/security/SecurityConfig.java
+++ src/main/java/com/login01/security/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
@@ -37,12 +39,10 @@ public class SecurityConfig {
 	    return configuration.getAuthenticationManager();
 	}
 	
-	
-	
-	
-	
-	
-	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	}
 	
 	
 }

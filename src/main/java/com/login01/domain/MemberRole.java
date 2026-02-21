26ccd4f86cc510e1986df42e6630b8f5dfda315b
Minho Kang <busster@naver.com>
2026-02-21 16:13:47 +0900

    회원 미리 넣어놓기 (tests파일에서 진행)

---
+++ src/main/java/com/login01/domain/MemberRole.java
@@ -0,0 +1,5 @@
+package com.login01.domain;
+
+public enum MemberRole {
+	USER,MANAGER,ADMIN
+}
---
+++ src/test/java/com/login01/MemberRepositoryTests.java
@@ -0,0 +1,31 @@
+package com.login01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.login01.domain.Member;
+import com.login01.repository.MemberRepository;
+
+@SpringBootTest
+public class MemberRepositoryTests {
+
+	@Autowired
+	private MemberRepository memberRepository;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Test
+	public void testInsertMember() {
+		for (int i = 0; i < 10; i++) {
+			Member member = Member.builder()
+					.email("user" + i + "@jjjj.com")
+					.password(passwordEncoder.encode("1234"))
+					.role("USER" + i)
+					.build();
+			memberRepository.save(member);
+		}
+	}
+
+}
